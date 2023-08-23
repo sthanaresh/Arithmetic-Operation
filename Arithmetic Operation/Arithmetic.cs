aad87381a275d7b4367852677752c1a65e8edc0e
sthanaresh <nareshshrestha1301@gmail.com>
2023-08-23 17:37:35 +0545

    Add project files.

---
+++ Arithmetic Operation/Arithmetic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_Operation
+{
+    public class Arithmetic
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y) 
+        {
+            return x - y;
+        }
+        public int Divide(int x, int y) 
+        {
+            return x / y;
+        }
+        public int Multiply(int x, int y) 
+        {
+            return x * y;
+        }
+        public int ModuloDivision(int x, int y) 
+        {
+            return x % y;
+        }
+
+        public string CheckEvenOdd(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return "It is even";
+            }
+            else
+            {
+                return "It is odd";
+            }
+        }
+    }  
+}
+
+
---
+++ Arithmetic Operation/Program.cs
@@ -0,0 +1,10 @@
+﻿using Arithmetic_Operation;
+
+Arithmetic operation= new Arithmetic();
+int x=18,y=3;
+Console.WriteLine($"The sum of {x} and {y} is {operation.Sum(x,y)}");
+Console.WriteLine($"The subtraction of {x} and {y} is {operation.Subtract(x, y)}");
+Console.WriteLine($"The division of {x} and {y} is {operation.Divide(x, y)}");
+Console.WriteLine($"The multiplication of {x} and {y} is {operation.Multiply(x, y)}");
+Console.WriteLine($"The modulo division of {x} and {y} is {operation.ModuloDivision(x, y)}");
+Console.WriteLine(operation.CheckEvenOdd(4));
